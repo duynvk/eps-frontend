6e55efc62bb675500cb6ca85f5aa3cfd37360f40
duynvk <83837747+duynvk@users.noreply.github.com>
2024-07-10 19:30:02 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://14.225.255.42:5000";
+const API_URL = "https://14.225.255.42:5000";
 
 export const login = async (credentials) => {
   const response = await axios.post(`${API_URL}/users/login`, credentials);
